4961ea0ff62d91fcd7c157d6b94c6e723fe29bcd
Sang. Nguyen Phu - CMC Global DU1.12 <npsang@cmcglobal.vn>
2022-04-18 17:14:49 +0700

    reate project

---
+++ src/main/java/com/example/ebshop/EbshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbshopApplication.class, args);
+	}
+
+}
